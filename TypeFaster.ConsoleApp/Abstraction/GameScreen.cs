adf9ac59a73c967b235babe0bb33b88140518201
Daniel Aguirre <uhyeay24@gmail.com>
2025-02-28 21:05:27 -0600

    Implemented GameScreen abstract base class and OpeningScreen/ClosingScreen.

---
+++ TypeFaster.ConsoleApp/Abstraction/GameScreen.cs
@@ -0,0 +1,21 @@
+﻿namespace TypeFaster.ConsoleApp.Abstraction
+{
+    /// <summary>
+    /// The GameScreen represents a 'Screen' that is presented to the user via the Console
+    /// </summary>
+    public abstract class GameScreen
+    {
+        /// <summary>
+        /// This method defines what happens when the screen is presented. 
+        /// When this method ends the GetNextScreen() will be called to determine what screen is presented next.
+        /// </summary>
+        public abstract void PresentScreen();
+
+        /// <summary>
+        /// This method defines what GameScreen the user should be presented next.
+        /// This method is called after PresentScreen() ends.
+        /// </summary>
+        /// <returns>GameScreen to be presented after the current GameScreen.</returns>
+        public abstract GameScreen GetNextScreen();
+    }
+}
